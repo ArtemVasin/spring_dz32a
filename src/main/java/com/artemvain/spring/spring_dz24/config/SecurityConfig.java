658a1358964bf538733d27fd0cb9c89fe0ece0e8
Wasin Artem <wartem87@gmail.com>
2022-05-04 12:45:44 +0400

    2

---
+++ src/main/java/com/artemvain/spring/spring_dz24/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.artemvain.spring.spring_dz24.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
